044df4bec0d466acc99ad4579eec7579e51f730e
gabrisattila <007gabrisati@gmail.com>
2023-08-26 23:16:08 +0200

    Todo change

--- src/main/java/classes/TODO.java
+++ src/main/java/classes/TODO.java
@@ -2,10 +2,9 @@ package classes;
 
 public class TODO {
 
-    //TODO Exception ha nem kompatibilis a méret és a megadott FEN
+    //TODO FEN converter kidolgozás
 
-    //TODO SwingUtilities beállítása. Már kezdeti indulásnál invokeLater-rel.
-    // Majd ha van Ai azt is
+
 
     //TODO Alap játék ablak egyszerű Human vs Ai és Ai vs Ai alapú működése. Szétszálazva.
 
