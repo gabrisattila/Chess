759d2986b64ffb11c9aa7a241602f18514f4d92b
gabris.attila <007gabrisati@gmail.com>
2023-08-28 15:53:08 +0200

    Started ViewFields rotation to be in good position

--- src/main/java/classes/GUI/FrameParts/GameBoard.java
+++ src/main/java/classes/GUI/FrameParts/GameBoard.java
@@ -1,5 +1,6 @@
 package classes.GUI.FrameParts;
 
+import classes.Game.I18N.Pair;
 import lombok.*;
 
 import javax.swing.*;
@@ -24,6 +25,7 @@ public class GameBoard extends JLayeredPane {
     public GameBoard(ViewBoard viewBoard){
         parentBoard = viewBoard;
         setBounds(300, 100, 8 * FIELD_WIDTH, 8 * FIELD_HEIGHT);
+
         for (var v : parentBoard.getFields()) {
             for (var vv : v) {
                 add(vv);
@@ -36,8 +38,25 @@ public class GameBoard extends JLayeredPane {
 
     //region Methods
 
-    public void setPieces(){
+    private void rotateBoard(){
+        Pair<Integer, Integer> xy = new Pair<>();
+        for (int i = 0; i < parentBoard.getFields().size(); i++) {
+            for (int j = 0; j < parentBoard.getFields().get(i).size(); j++) {
+                //tmp = matrix[i][j]
+                xy.setFirst(parentBoard.getFieldByIJFromBoard(i, j).getX());
+                xy.setSecond(parentBoard.getFieldByIJFromBoard(i, j).getY());
+                //matrix[i][j] = matrix[j][i]
+                setNewBounds(i, j,
+                        parentBoard.getFieldByIJFromBoard(j, i).getX(),
+                        parentBoard.getFieldByIJFromBoard(j, i).getY());
+                //matrix[j][i] = tmp
+                setNewBounds(j, i, xy.getFirst(), xy.getSecond());
+            }
+        }
+    }
 
+    private void setNewBounds(int X, int Y, int newX, int newY){
+        parentBoard.getFieldByIJFromBoard(X, Y).setBounds(newX, newY, FIELD_WIDTH, FIELD_HEIGHT);
     }
 
     //endregion
