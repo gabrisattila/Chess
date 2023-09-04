fe306407da9e925b77f7162fb2aea211e4bb602f
gabris.attila <007gabrisati@gmail.com>
2023-09-04 08:57:03 +0200

    Ai vs Ai automatically works

--- src/main/java/classes/TODO.java
+++ src/main/java/classes/TODO.java
@@ -3,8 +3,6 @@ package classes;
 public class TODO {
 
 
-    //TODO Ai vs Ai működése.
-
     //TODO gondolkozni, hogy a generikus field-ezés helyett interface-t érdemes-e használni.
 
     //TODO Szabályos lépések megírása
