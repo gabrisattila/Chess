3830066d7bf0dcf16aec8b4d99272ef6dd6585a9
gabrisattila <007gabrisati@gmail.com>
2023-09-05 22:09:07 +0200

    Todo update!! Holnap melóban vagy ez vagy dokumentáció.

--- src/main/java/classes/TODO.java
+++ src/main/java/classes/TODO.java
@@ -2,9 +2,9 @@ package classes;
 
 public class TODO {
 
-    //TODO Színválasztó ComboBox with buttons
-
     //TODO gondolkozni, hogy a generikus field-ezés helyett interface-t érdemes-e használni.
+
+    //TODO Színválasztó ComboBox with buttons
 
     //TODO Még kiderül jó-e -> Wait notify az EDT-be
 
