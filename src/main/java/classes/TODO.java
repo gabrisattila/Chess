814cea093f03311cf8c89bb62366281dfaab65ea
gabrisattila <007gabrisati@gmail.com>
2023-09-25 22:39:56 +0200

    TODO Sufni tuning megoldás Világosra miért nem megy?

--- src/main/java/classes/TODO.java
+++ src/main/java/classes/TODO.java
@@ -2,9 +2,10 @@ package classes;
 
 public class TODO {
 
-    //TODO Miért csak úgy működik a piece-ken való végig iterálás, hogy IPiece-n
-    // megyek végig nem pedig a táblán és úgy kérem le a piece-t?
-    // Mert ez később elég nagy baj lehet. Nem tudok jól lekérni piece object-t!!
+    //TODO Miért nem megy a sufni tuning javítás fehér AI-ra
+    // rajzzal megérteni!!!
+
+    //TODO Javítani a figura lépés választás kattintás if-jén
 
     //TODO Színválasztó ComboBox with buttons
 
