f9ba8f6d25e71a142e0ccc5d8df7c517bbf6f5ca
gabrisattila <007gabrisati@gmail.com>
2023-10-04 22:57:50 +0200

    Castle Check bevezetése a FenConverter-be, todo hiba javítás

--- src/main/java/classes/TODO.java
+++ src/main/java/classes/TODO.java
@@ -2,7 +2,7 @@ package classes;
 
 public class TODO {
 
-
+    //TODO Javítani a fenConverter index hibáját
 
     //TODO !!!
     // Minimax alatt nem elfelejteni A Castle és az EmPassant helyzeteket lekezelni
