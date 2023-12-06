d55ddbdcab1bc2b4669e5448c23ad7469754e00e
gabrisattila <007gabrisati@gmail.com>
2023-12-06 09:05:11 +0100

    k

--- src/main/java/classes/Main.java
+++ src/main/java/classes/Main.java
@@ -1,16 +1,19 @@
 package classes;
 
+import classes.Ai.FenConverter;
 import classes.Game.I18N.ChessGameException;
 import classes.Game.Model.Logic.EDT;
 
 import javax.swing.*;
 
 import static classes.Game.I18N.METHODS.exceptionIgnorer;
+import static classes.Game.I18N.VARS.FINALS.usualFens;
 
 public class Main {
 
     public static void main(String[] args) {
 //           exceptionIgnorer();
         new EDT();
+
     }
 }
